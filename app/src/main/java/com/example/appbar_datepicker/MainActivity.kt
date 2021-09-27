833fb39253fa9548f2da9ecddb7b1de37556c57b
MARIA LUIZA RIBEIRO TEIXEIRA <21193063@CFP127ALUNO.LOCAL>
2021-09-27 09:42:38 -0300

    Primeiro commit - nada feito ainda

---
+++ app/src/main/java/com/example/appbar_datepicker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appbar_datepicker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
